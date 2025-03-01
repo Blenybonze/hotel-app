99f15d720f58156437d0de07498f458347c007bb
Bleny Bonze <blenybonze@hotmail.com>
2025-03-01 18:16:08 -0300

    project estructure completed.

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReservationComponent } from './reservation.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
 
 
 
 @NgModule({
   declarations: [
-    ReservationComponent
+    ReservationFormComponent,
+    ReservationListComponent
   ],
   imports: [
     CommonModule
