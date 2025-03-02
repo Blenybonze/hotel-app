4ef257666acd95b3d76674613709a38fc8229397
Bleny Bonze <blenybonze@hotmail.com>
2025-03-02 18:34:13 -0300

    add filter name and filter roomNumber

--- src/app/reservation/reservation-list/reservation-list.component.ts
+++ src/app/reservation/reservation-list/reservation-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReservationService } from '../reservation.service';
 import { Reservation } from 'src/app/models/reservation';
 import { Router } from '@angular/router';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-reservation-list',
@@ -11,6 +12,10 @@ import { Router } from '@angular/router';
 export class ReservationListComponent implements OnInit {
 
   reservations: Reservation[] = []
+  filterReservations: Reservation[] = []
+
+  guestNameFilter: string = '';
+  roomNumberFilter: string = '';
 
   constructor(
     private reservationService: ReservationService,
@@ -21,6 +26,7 @@ export class ReservationListComponent implements OnInit {
 
   ngOnInit(): void {
     this.reservations = this.reservationService.getReservations();
+    this.filterReservations = this.reservations;
   }
 
   deleteReservation(id: string) {
@@ -30,4 +36,27 @@ export class ReservationListComponent implements OnInit {
   goToCreate() {
     this.router.navigate(['/new'])
   }
+
+
+  applyFilter() {
+    let filteredReservations = [...this.reservations]; // Inicia com todas as reservas
+
+    // Aplica o filtro de guestName se o valor estiver preenchido
+    if (this.guestNameFilter && this.guestNameFilter.length > 0) {
+      filteredReservations = filteredReservations.filter(res =>
+        res.guestName.toLowerCase().includes(this.guestNameFilter.toLowerCase())
+      );
+    }
+
+    // Aplica o filtro de roomNumber se o valor estiver preenchido
+    if (this.roomNumberFilter && this.roomNumberFilter.length > 0) {
+      filteredReservations = filteredReservations.filter(res =>
+        res.roomNumber.toString().toLowerCase().includes(this.roomNumberFilter.toLowerCase())
+      );
+    }
+
+    // Atualiza a lista filtrada com base nos filtros aplicados
+    this.filterReservations = filteredReservations;
+  }
+
 }
